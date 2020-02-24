c3115be5fce4b059b094d30d0f736b6bbd0860fa
Dmitry Kazakov <dmitriy.kazakov.da@gmail.com>
2020-02-24 17:49:35 +0300

    Add platform dependent scenario for probe test

--- app/src/test/java/com/coderbunker/assistant/ProbeTest.java
+++ app/src/test/java/com/coderbunker/assistant/ProbeTest.java
@@ -7,6 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.RuntimeEnvironment;
 
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
 public class ProbeTest extends BaseTest {
 
     private Repository repository;
@@ -19,5 +23,22 @@ public class ProbeTest extends BaseTest {
     @Test
     public void testSampleCase() {
         Assert.assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testPlatformDependentCode() {
+        int widgetId = 10;
+        repository.saveData(widgetId, getData());
+
+        ArrayList<String> data = repository.getData();
+        assertEquals(3, data.size());
+    }
+
+    private ArrayList<String> getData() {
+        ArrayList<String> result = new ArrayList<>();
+        result.add("one");
+        result.add("two");
+        result.add("three");
+        return result;
     }
 }
