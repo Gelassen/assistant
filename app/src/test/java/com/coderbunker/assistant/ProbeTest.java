c99c39401801c4947c4632c3d482fa767ec49ac7
Dmitry Kazakov <dmitriy.kazakov.da@gmail.com>
2020-02-24 17:27:06 +0300

    Add probe robolectric test to localise issue

---
+++ app/src/test/java/com/coderbunker/assistant/ProbeTest.java
@@ -0,0 +1,12 @@
+package com.coderbunker.assistant;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProbeTest extends BaseTest {
+
+    @Test
+    public void testSampleCase() {
+        Assert.assertEquals(2, 1 + 1);
+    }
+}
