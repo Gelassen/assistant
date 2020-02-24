c6f731492f7ad1566a224d18e3552858e604e60b
Dmitry Kazakov <dmitriy.kazakov.da@gmail.com>
2020-02-24 17:34:41 +0300

    Add platform independent dependency for probe test

--- app/src/test/java/com/coderbunker/assistant/ProbeTest.java
+++ app/src/test/java/com/coderbunker/assistant/ProbeTest.java
@@ -1,9 +1,20 @@
 package com.coderbunker.assistant;
 
+import com.coderbunker.assistant.widget.Repository;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.robolectric.RuntimeEnvironment;
 
 public class ProbeTest extends BaseTest {
+
+    private Repository repository;
+
+    @Before
+    public void setUp() {
+        repository = new Repository(RuntimeEnvironment.application);
+    }
 
     @Test
     public void testSampleCase() {
