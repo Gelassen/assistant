99e8bb9182662b10ca0149a36f8a52b5ed531e01
Dmitry Kazakov <dmitriy.kazakov.da@gmail.com>
2020-02-25 14:31:58 +0300

    Integration test via espresso

--- app/src/main/java/com/coderbunker/assistant/MainActivity.java
+++ app/src/main/java/com/coderbunker/assistant/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.coderbunker.assistant.currency.CurrencyProvider;
 import com.coderbunker.assistant.currency.CurrencyService;
@@ -24,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.clickTheButton)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, getString(R.string.toast_open_the_app), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
